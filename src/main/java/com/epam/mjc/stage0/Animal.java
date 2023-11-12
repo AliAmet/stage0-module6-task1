0925266876e5ff91a5f866a63f4791f40d26c463
AliAmet <ametov1ali@gmail.com>
2023-11-12 21:38:03 +0600

    first commit

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    String getDescription(){
+        if (hasFur == true) {
+            return "This animal is mostly " +  color +". It has " +  numberOfPaws +
+                    " paws and a fur";
+        } else{
+            return "This animal is mostly " +  color +". It has " +  numberOfPaws +
+                    " paws and no fur";
+        }
+    }
 }

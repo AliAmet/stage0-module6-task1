163204f865e3aec6665d51a5b2cff42d0d6c427c
AliAmet <ametov1ali@gmail.com>
2023-11-19 12:19:06 +0600

    first commit

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -6,7 +6,7 @@ public class Bird extends Animal {
     }
 
     public Bird(String color, int numberOfPaws, boolean hasFur) {
-        super("blue", 2, false);
+        super(color, numberOfPaws, hasFur);
     }
 
     @Override
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -6,7 +6,7 @@ public class Dog extends Animal{
     }
 
     public Dog(String color, int numberOfPaws, boolean hasFur) {
-        super("brown", 4, false);
+        super(color, numberOfPaws, hasFur);
     }
 
 }
